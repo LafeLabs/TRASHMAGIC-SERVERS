644351229ef58c8da2d6bb90a142120f218b3c27
Trash Robot <lafelabs@gmail.com>
2025-02-18 14:33:52 -0700

    SELF REPLICATING WEB SERVERS MADE OUT OF TRASH!

---
+++ XAMPP/local-replicator.php
@@ -0,0 +1,49 @@
+<?php
+
+$dnaraw = file_get_contents("../data/dna.txt");
+$dna = json_decode($dnaraw);
+$baseurl = "../";
+
+mkdir("data");
+mkdir("php");
+mkdir("freestore");
+
+copy("../php/local-replicator.txt","replicator.php");
+
+foreach($dna->html as $value){
+    
+    copy($baseurl.$value,$value);
+
+}
+
+
+foreach($dna->data as $value){
+    
+    copy($baseurl."data/".$value,"data/".$value);
+    
+}
+
+
+foreach($dna->php as $value){
+ 
+    copy($baseurl."php/".$value,"php/".$value);
+    copy($baseurl."php/".$value,explode(".",$value)[0].".php");
+
+}
+    
+
+
+?>
+<a href = "index.html">CLICK ME(3/3)</a>
+<style>
+body{
+    background-color:BLACK;
+    font-family:Comic Sans MS;
+    font-size:3em;
+}
+a{
+        font-size:3em;
+        color:#ff2cb4;
+
+}
+</style>
